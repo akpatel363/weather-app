302247e44c18a2f193550a00bb92c8dee347f742
Akash Patel <akpatel363@gmail.com>
2019-11-22 14:40:41 +0530

    Error Handling Modified

--- public/js/favourites.js
+++ public/js/favourites.js
@@ -1,25 +1,24 @@
 var cardContainer = document.querySelector('.card-container')
 var favString = localStorage.getItem('favourites')
 var favourites = []
-if(favString==null)
-{
+if (favString == null) {
     displayNoItems()
-}else{
+} else {
     favourites = JSON.parse(favString)
-    if(favourites.length==0){
+    if (favourites.length == 0) {
         displayNoItems()
-    }else{
+    } else {
         addItem(0)
     }
 }
 
-async function addItem(index){
+async function addItem(index) {
     var item = favourites[index]
     var card = document.createElement('div')
     card.classList.add('card')
     var closeIcon = document.createElement('i')
     closeIcon.className = 'fa fa-times close-icon'
-    closeIcon.addEventListener('click',()=>{
+    closeIcon.addEventListener('click', () => {
         deleteData(index)
         card.remove()
     })
@@ -33,30 +32,34 @@ async function addItem(index){
     text.innerHTML = "Loading. Please Wait"
     card.appendChild(text)
     cardContainer.appendChild(card)
-    text.innerHTML = await getWeatherDetails(text,item['name'])
-    if(index<favourites.length-1){
+    text.innerHTML = await getWeatherDetails(text, item['name'])
+    if (index < favourites.length - 1) {
         addItem(++index)
     }
 }
 
-function displayNoItems(){
+function displayNoItems() {
     var message = document.createElement('h2')
     message.className = 'error-message'
     message.innerHTML = 'No Favourites Found.'
     cardContainer.appendChild(message)
 }
 
-async function getWeatherDetails(textElement,location){
-    var res = await fetch('/weather?search='+encodeURIComponent(location))
-    var json = await res.json()
-    if(json.error)
-        return json.error
-    return json.forecast
+async function getWeatherDetails(textElement, location) {
+    try {
+        var res = await fetch('/weather?search=' + encodeURIComponent(location))
+        var json = await res.json()
+        if (json.error)
+            return json.error
+        return json.forecast
+    } catch (error) {
+        return "Unable to connect to server. Please check your internet connection."
+    }
 }
-function deleteData(index){
-    favourites.splice(index-1,1)
-    localStorage.setItem('favourites',JSON.stringify(favourites))
-    if(favourites.length==0){
+function deleteData(index) {
+    favourites.splice(index - 1, 1)
+    localStorage.setItem('favourites', JSON.stringify(favourites))
+    if (favourites.length == 0) {
         displayNoItems()
     }
 }
